4b3f521bc0dcd541997509f210024f0587957e06
Tkachenko AI <alexfisher13@yandex.ru>
2018-02-13 16:37:47 +0300

    add http request service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
 import { AppComponent } from './app.component';
+import { StatComponent } from './stat/stat.component';
+import {HttpModule} from '@angular/http';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StatComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
