de3db9a73829db31f074f362e4eef327be39ef46
Tkachenko AI <alexfisher13@yandex.ru>
2018-02-14 17:08:20 +0300

    add parsing json 2

--- src/app/vkapi.service.ts
+++ src/app/vkapi.service.ts
@@ -7,7 +7,7 @@ export class VkapiService {
   constructor(private httpClient: HttpClient) {}
 
   getUser(): Observable<any> {
-    return this.httpClient.get('https://api.vk.com/method/users.get?user_ids=17261895', {
+    return this.httpClient.get('https://api.vk.com/method/users.get?user_ids=y.kudinova', {
       observe: 'body',
       responseType: 'json'
     });
